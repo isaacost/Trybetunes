c58e3d51a0df773b033fdf17eb7c837072f546b2
Isa Costa <isa-hcosta@hotmail.com>
2022-08-31 15:41:16 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
+
+const MIN_LENGTH = 3;
 
 class Login extends React.Component {
+  state = {
+    nome: '',
+    loading: false,
+    logado: false,
+  };
+
+  getName = (event) => {
+    const { value } = event.target;
+    this.setState({
+      nome: value,
+    });
+  };
+
+  activateBtn = () => {
+    const { nome } = this.state;
+    return nome.length < MIN_LENGTH;
+  };
+
+  handleClick = async () => {
+    const { nome } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: nome });
+    this.setState({
+      loading: false,
+      logado: true,
+    });
+  };
+
   render() {
+    const { nome, loading, logado } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <label htmlFor="name-input">
+          Nome:
+          <input
+            type="text"
+            data-testid="login-name-input"
+            id="name-input"
+            value={ nome }
+            onChange={ this.getName }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          onClick={ this.handleClick }
+          disabled={ this.activateBtn() }
+        >
+          Entrar
+        </button>
+        {loading && (
+          <Loading />
+        )}
+        {logado && (
+          <Redirect to="/search" />
+        )}
+
+      </div>
     );
   }
 }
