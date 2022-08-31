0a315dd58bedc4c64fdf37d1fa91c39432e0cb9e
Isa Costa <isa-hcosta@hotmail.com>
2022-08-31 18:24:25 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const MIN_LENGTH = 2;
+
 class Search extends React.Component {
+  state = {
+    artist: '',
+  };
+
+  getArtist = (event) => {
+    const { value } = event.target;
+    this.setState({
+      artist: value,
+    });
+  };
+
+  activateBtn = () => {
+    const { artist } = this.state;
+    return artist.length < MIN_LENGTH;
+  };
+
   render() {
+    const { artist } = this.state;
     return (
-      <div>
+      <div data-testid="page-search">
         <Header />
-        <div data-testid="page-search" />
+        <label htmlFor="search-artist-input">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search-artist-input"
+            value={ artist }
+            onChange={ this.getArtist }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ this.activateBtn() }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
