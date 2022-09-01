83cc154a0b58a6006b394d6f3b6aa89758abf918
Isa Costa <isa-hcosta@hotmail.com>
2022-09-01 19:11:38 -0300

    requisito 11 e arruma requisito 9

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,75 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
-import Loading from './Loading';
 
 class MusicCard extends React.Component {
-  state = {
-    loading: false,
-    favorites: [],
-  };
-
-  async componentDidMount() {
-    this.setState({
-      favorites: await getFavoriteSongs(),
-    });
-  }
-
-  handleClick = async (track) => {
-    this.setState({
-      loading: true,
-    });
-    await addSong(track);
-    const { favorites } = this.state;
-    this.setState({
-      favorites: [...favorites, track],
-      loading: false,
-    });
-  };
-
   render() {
-    const { track } = this.props;
-    const { trackName, previewUrl, trackId } = track;
-    const { favorites, loading } = this.state;
-    const favoriteMusics = favorites.some((song) => trackId === song.trackId);
+    const { value, handleClick, checked } = this.props;
+    const { trackName, previewUrl, trackId } = value;
     return (
       <div>
-        {
-          loading
-            ? (<Loading />)
-            : (
-              <div>
-                {' '}
-                <p>
-                  {trackName}
-                </p>
-                <audio data-testid="audio-component" src={ previewUrl } controls>
-                  <track kind="captions" />
-                </audio>
-                <label htmlFor="favorita">
-                  Favorita
-                  <input
-                    type="checkbox"
-                    data-testid={ `checkbox-music-${trackId}` }
-                    id={ trackId }
-                    defaultChecked={ favoriteMusics }
-                    onClick={ () => this.handleClick(track) }
-                  />
-                </label>
-              </div>
-            )
-        }
-
+        <p>
+          {trackName}
+        </p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+        </audio>
+        <label htmlFor={ trackId }>
+          Favorita
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+            id={ trackId }
+            defaultChecked={ checked }
+            onClick={ handleClick }
+          />
+        </label>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  track: PropTypes.shape({
-    trackName: PropTypes.string.isRequired,
-    previewUrl: PropTypes.string.isRequired,
-    trackId: PropTypes.number.isRequired,
+  value: PropTypes.shape({
+    trackName: PropTypes.string,
+    previewUrl: PropTypes.string,
+    trackId: PropTypes.number,
   }).isRequired,
+  checked: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
+import Loading from '../components/Loading';
 import getMusics from '../services/musicsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   state = {
@@ -10,6 +12,8 @@ class Album extends React.Component {
     artistName: '',
     collectionName: '',
     artworkUrl100: '',
+    loading: false,
+    favorites: [],
   };
 
   componentDidMount() {
@@ -18,35 +22,79 @@ class Album extends React.Component {
 
   getMusics = async () => {
     const { match: { params: { id } } } = this.props;
+    this.setState({
+      loading: true,
+    });
     const resposta = await getMusics(id);
+    const favorites = await getFavoriteSongs();
     this.setState({
       musics: resposta.slice(1),
       artistName: resposta[0].artistName,
       collectionName: resposta[0].collectionName,
       artworkUrl100: resposta[0].artworkUrl100,
+      favorites,
+      loading: false,
+    });
+  };
+
+  handleClick = async ({ target }) => {
+    const { musics } = this.state;
+    const song = musics.find(({ trackId }) => trackId === +target.id);
+    this.setState({
+      loading: true,
+    }, async () => {
+      if (target.checked) {
+        await addSong(song);
+      } else {
+        await removeSong(song);
+      }
+      const favorites = await getFavoriteSongs();
+      this.setState({
+        favorites,
+        loading: false,
+      });
     });
   };
 
   render() {
-    const { musics, artistName, collectionName, artworkUrl100 } = this.state;
+    const {
+      musics,
+      artistName,
+      collectionName,
+      artworkUrl100,
+      loading,
+      favorites } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <img src={ artworkUrl100 } alt={ collectionName } />
-        <h2 data-testid="artist-name">
-          {artistName}
-        </h2>
-        <h3 data-testid="album-name">
-          {collectionName}
-        </h3>
         {
-          musics.map((music) => (<MusicCard
-            key={ music.trackId }
-            track={ music }
-            trackName={ music.trackName }
-            previewUrl={ music.previewUrl }
-          />))
+          loading
+            ? (<Loading />)
+            : (
+              <div>
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <h2 data-testid="artist-name">
+                  {artistName}
+                </h2>
+                <h3 data-testid="album-name">
+                  {collectionName}
+                </h3>
+                {
+                  musics.map((music) => (<MusicCard
+                    key={ music.trackId }
+                    track={ music }
+                    trackName={ music.trackName }
+                    previewUrl={ music.previewUrl }
+                    value={ music }
+                    checked={ favorites
+                      .some(({ trackId }) => +trackId === +music.trackId) }
+                    handleClick={ this.handleClick }
+                  />))
+                }
+              </div>
+            )
         }
+
       </div>
     );
   }
